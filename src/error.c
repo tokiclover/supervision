a9a079394da0bb6cf5399b9dba322d43eae2b51a
tokiclover <tokiclover@gmail.com>
2016-05-04 12:47:45 +0200

    Added src/error.c (error handling)

---
+++ src/error.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2016 tokiclover <tokiclover@gmail.com>
+ * This file is part of Supervision (Scripts Framework).
+ *
+ * The supervision framework is free software; you can redistribute
+ * it and/or modify it under the terms of the 2-clause, simplified,
+ * new BSD License included in the distriution of this package.
+ */
+
+#include "error.h"
+
+void error(int err, const char *fmt, ...)
+{
+	char buf[BUFSIZ];
+	va_list va;
+	va_start(va, fmt);
+
+	vsnprintf(buf, BUFSIZ-1, fmt, va);
+	if (errno) {
+		int len = strlen(buf);
+		snprintf(buf+len, BUFSIZ-len-1, ": %s", strerror(errno));
+	}
+	fprintf(stderr, "%s\n", buf);
+	fflush(NULL);
+
+	va_end(va);
+	if (err < 0)   abort();
+	if (err > 0) exit(err);
+}
+
+void *err_malloc(size_t size)
+{
+	void *ptr = malloc(size);
+	if (ptr)
+		return ptr;
+	ERR_EXIT;
+}
+
+void *err_calloc(size_t num, size_t size)
+{
+	void *ptr = calloc(num, size);
+	if (ptr)
+		return ptr;
+	ERR_EXIT;
+}
+
+void *err_realloc(void *ptr, size_t size)
+{
+	ptr = realloc(ptr, size);
+	if (ptr)
+		return ptr;
+	ERR_EXIT;
+}
+
+char *err_strdup(const char* str)
+{
+	char *ptr = strdup(str);
+	if (!str)
+		return NULL;
+	if (ptr)
+		return ptr;
+	ERR_EXIT;
+}
+
