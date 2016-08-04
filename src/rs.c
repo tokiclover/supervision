596491f76bced19dc266422e996fe78d12422061
tokiclover <tokiclover@gmail.com>
2016-08-04 18:34:59 +0200

    rs: Removed unnecessary svc_runlevel() function and softlevel record

--- src/rs.c
+++ src/rs.c
@@ -176,12 +176,6 @@ static int svc_mark(const char *svc, int status);
 static void svc_level(void);
 static char *get_cmdline_entry(const char *entry);
 
-/* simple helper to set/get runlevel
- * @level: runlevel to set, or NULL to get the current runlevel;
- * @return: current runlevel;
- */
-static char *svc_runlevel(const char *level);
-
 /*
  * lock file for service to start/stop
  * @svc: service name;
@@ -650,45 +644,6 @@ static char *get_cmdline_entry(const char *ent)
 #else
 	return NULL;
 #endif /* __linux__ */
-}
-
-static char *svc_runlevel(const char *level)
-{
-	static char b[16], p[] = SV_TMPDIR "/softlevel";
-	char *r;
-	int fd, f = O_NONBLOCK|O_CREAT;
-
-	if (level)
-		f |= O_TRUNC|O_WRONLY;
-	else if (access(p, F_OK))
-		return NULL;
-	else
-		f |= O_RDONLY;
-	if ((fd = open(p, f, 0644)) < 0) {
-		WARN("Failed to open `%s': %s\n", p, strerror(errno));
-		return NULL;
-	}
-
-	if (level) {
-		f = strlen(level);
-		if (write(fd, level, f) < f) {
-			WARN("Failed to write to `%s': %s\n", p, strerror(errno));
-			r = NULL;
-		}
-		else
-			r = (char*)level;
-	}
-	else {
-		if (( f = read(fd, b, sizeof(b))) < 0) {
-			WARN("Failed to read from `%s': %s\n", p, strerror(errno));
-			r = NULL;
-		}
-		else
-			b[f] = '\0', r = b;
-	}
-
-	close(fd);
-	return r;
 }
 
 static void svc_level(void)
@@ -1164,10 +1119,6 @@ static void svc_stage(const char *cmd)
 	/* finish sysinit */
 	if (rs_stage == 0 )
 		svc_stage_command(0, argc, argv, envp);
-	if (rs_runlevel > 0)
-		svc_runlevel(rs_stage_name[rs_runlevel]);
-	else
-		svc_runlevel(rs_stage_name[rs_stage]);
 
 	t = time(NULL);
 	svc_log("\nrs init stage-%d stopped at %s\n", rs_stage, ctime(&t));
