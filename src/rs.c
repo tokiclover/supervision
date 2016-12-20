fa7ecdffc34746ef3e0ecacce4ca46944e37b2b1
tokiclover <tokiclover@gmail.com>
2016-12-20 21:52:07 +0100

    rs: 0.12.6.4 version bump

--- src/rs.c
+++ src/rs.c
@@ -20,7 +20,7 @@
 #include <poll.h>
 #include <time.h>
 
-#define VERSION "0.12.6"
+#define VERSION "0.12.6.4"
 #define RS_RUNSCRIPT SV_LIBDIR "/sh/runscript"
 
 #define SV_TMPDIR_DOWN SV_TMPDIR "/down"
