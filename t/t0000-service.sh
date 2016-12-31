d26e1d649d6e35f9cbb9454292d3635ff656dd26
tokiclover <tokiclover@gmail.com>
2016-12-31 13:46:19 +0100

    t/t0000-service.sh: clean up test cases

--- t/t0000-service.sh
+++ t/t0000-service.sh
@@ -8,19 +8,5 @@ printf "bogus usage\n"
 rs stage
 printf "\n"
 
-runscript=/lib/sv/sh/runscript
-mv ${runscript}{,-} || exit
-
-cat <<EOF >$runscript
-#!/bin/sh
-echo "\$@"
-exit 1
-EOF
-chmod +x $runscript
-printf "starting stage-0\n"
-rs -0 stage
-printf "\ndefault runlevel\n"
-rs default
-
-mv -f ${runscript}{-,} || exit
-
+ln -fs rs src/sv-stage
+src/sv-stage --default status
