52570ee41a3355188a8b2ebed6e750713502c26f
tokiclover <tokiclover@gmail.com>
2017-01-23 13:24:59 +0100

    t/t0000-service.sh: sync to v0.13.0

--- t/t0000-service.sh
+++ t/t0000-service.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-sv/sysfs status
+ln -fs ../../../src/rs sv/.lib/sbin/service
+sv/.lib/sbin/service sysfs status
 sv/sysfs start
 sv/atd/run status
 
 printf "bogus usage\n"
-rs stage
+src/rs stage
 printf "\n"
 
 ln -fs rs src/sv-stage
