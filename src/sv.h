df83ac38e2d9418c61c19021d1b906fc63725b71
tokiclover <tokiclover@gmail.com>
2018-07-24 00:46:12 +0200

    sv-{run,init}: v0.14.0 version bump

--- src/sv.h
+++ src/sv.h
@@ -17,7 +17,7 @@
 	"This is free software: you are free to change and distribute it.\n" \
 	"There is NO WARANTY, to the extend permitted by law.\n"
 
-#define SV_VERSION "0.13.0"
+#define SV_VERSION "0.14.0"
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
