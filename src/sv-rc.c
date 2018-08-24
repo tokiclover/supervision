dcb9025de251900d2a70c715c34d0fcadd3a7b02
tokiclover <tokiclover@gmail.com>
2018-08-24 14:12:20 +0200

    sv-rc: svc_init_level: fix DBG arguments

--- src/sv-rc.c
+++ src/sv-rc.c
@@ -492,7 +492,7 @@ static int svc_init_level(int argc, const char *argv[])
 	SV_SvcDeps_T *dep;
 	SV_String_T *svc;
 #ifdef SV_DEBUG
-	if (sv_debug) DBG("%s(%d, %d, %p)\n", __func__, argc, argv);
+	if (sv_debug) DBG("%s(%d, %p)\n", __func__, argc, argv);
 #endif
 
 	if (sv_init == SV_SHUTDOWN_LEVEL)
