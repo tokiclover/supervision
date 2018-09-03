8a56bdbb1ccc44839e90d665f0ae676d6d8affec
tokiclover <tokiclover@gmail.com>
2018-09-03 23:00:10 +0200

    add src/sv-copyright.h

--- src/sv.h
+++ src/sv.h
@@ -12,10 +12,13 @@
 #ifndef SV_H
 #define SV_H
 
+#include "sv-copyright.h"
+#ifndef SV_COPYRIGHT_H
 #define SV_COPYRIGHT "@(#) Copyright (c) 2015-2018 tokiclover <tokiclover@gmail.com>\n" \
 	"License 2-clause, new, simplified BSD or MIT (at your name choice)\n" \
 	"This is free software: you are free to change and distribute it.\n" \
 	"There is NO WARANTY, to the extend permitted by law.\n"
+#endif /* SV_COPYRIGHT_H */
 
 #define SV_VERSION "0.14.0"
 
