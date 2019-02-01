03da9549933ca46f1f3cfb0142b780265a95b9fd
tokiclover <tokiclover@gmail.com>
2019-02-01 22:52:40 +0100

    added 2019 to copyright

--- src/sv-copyright.h
+++ src/sv-copyright.h
@@ -1,5 +1,5 @@
 #ifndef SV_COPYRIGHT_H
-#define SV_COPYRIGHT "@(#) Copyright (c) 2015-2018 tokiclover <tokiclover@gmail.com>\n" \
+#define SV_COPYRIGHT "@(#) Copyright (c) 2015-2019 tokiclover <tokiclover@gmail.com>\n" \
 	"License 2-clause, new, simplified BSD or MIT (at your name choice)\n" \
 	"This is free software: you are free to change and distribute it.\n" \
 	"There is NO WARANTY, to the extend permitted by law.\n"
